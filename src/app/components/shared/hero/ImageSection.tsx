a311a235316453cc6e044a4a0a2fd0677b06ff44
Stivenjs <stevenjaime164q@gmail.com>
2024-12-20 00:51:59 -0500

    Se quito la animacion en movil para mas optimizacion

--- src/app/components/shared/hero/ImageSection.tsx
+++ src/app/components/shared/hero/ImageSection.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { GridOverlay } from "./GridOverlay";
 import { useScrollEffect } from "@/app/hooks/useScrollEffect";
+
 const ImageSection = () => {
   const { opacity, transform } = useScrollEffect();
 
   return (
-    <div className="relative w-full lg:w-1/2 h-screen overflow-hidden">
+    <div className="relative w-full lg:w-1/2 h-auto lg:h-screen overflow-hidden">
+      {/* Ocultar el fondo en dispositivos móviles */}
       <div
-        className="absolute inset-0 bg-gradient-to-br from-violet-900/90 to-fuchsia-900/90 mix-blend-multiply"
+        className="absolute inset-0 bg-gradient-to-br from-violet-900/90 to-fuchsia-900/90 mix-blend-multiply hidden lg:block"
         style={{ opacity }}
       />
       <div
@@ -17,10 +19,11 @@ const ImageSection = () => {
           transition: "transform 0.1s linear",
         }}
       >
+        {/* Ocultar imagen en dispositivos móviles */}
         <img
           src="https://images.unsplash.com/photo-1581090700227-1e37b190418e?auto=format&fit=crop&q=80"
           alt="Tech Innovation"
-          className="h-full w-full object-cover object-center"
+          className="h-full w-full object-cover object-center hidden lg:block"
           style={{ transform: "scale(1.1)" }}
         />
       </div>
