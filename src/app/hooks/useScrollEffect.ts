bfe49e34797ec724687600308f7a8b8895662e6e
Stivenjs <stevenjaime164q@gmail.com>
2024-12-20 00:39:45 -0500

    Se quito la animacion en movil para mas optimizacion

--- src/app/hooks/useScrollEffect.ts
+++ src/app/hooks/useScrollEffect.ts
@@ -1,44 +1,48 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useScrollEffect = () => {
   const [scrollY, setScrollY] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Verificar si el ancho de la pantalla es menor a 768px (dispositivo móvil)
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
     };
 
-    // Inicializar el estado de mobile en función del tamaño de la ventana
-    handleResize();
-
-    // Escuchar cambios en el tamaño de la ventana
+    handleResize(); // Inicializar en función del tamaño de la ventana
     window.addEventListener("resize", handleResize);
 
-    const handleScroll = () => {
-      // Solo actualizamos el scrollY si no es un dispositivo móvil
-      if (!isMobile) {
-        setScrollY(window.scrollY);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll, { passive: true });
-
     return () => {
-      window.removeEventListener("scroll", handleScroll);
       window.removeEventListener("resize", handleResize);
     };
+  }, []);
+
+  const handleScroll = useCallback(() => {
+    if (!isMobile) {
+      setScrollY(window.scrollY);
+    }
   }, [isMobile]);
 
+  useEffect(() => {
+    const throttledScroll = () => {
+      requestAnimationFrame(handleScroll); // Asegura que el scroll se maneje eficientemente
+    };
+
+    window.addEventListener("scroll", throttledScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", throttledScroll);
+    };
+  }, [handleScroll]);
+
   const calculateOpacity = () => {
-    if (isMobile) return 1; // No aplicar efecto de opacidad en móvil
+    if (isMobile) return 1; // No aplicar efecto en móviles
     const maxScroll = window.innerHeight * 0.3;
     return Math.max(0, 1 - scrollY / maxScroll);
   };
 
   const calculateTransform = () => {
-    if (isMobile) return "translateY(0px) scale(1)"; // No aplicar transformaciones en móvil
+    if (isMobile) return "translateY(0px) scale(1)"; // No aplicar transformaciones en móviles
     const transform = scrollY * 0.3;
     return `translateY(${transform}px) scale(${1 + scrollY * 0.0005})`;
   };
