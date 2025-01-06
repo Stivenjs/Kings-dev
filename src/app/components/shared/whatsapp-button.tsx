d9066e2353c0ec4ba23c8ade598bfc130b0b2611
Stivenjs <stevenjaime164q@gmail.com>
2025-01-06 16:57:57 -0500

    Integracion de chat bot

--- src/app/components/shared/whatsapp-button.tsx
+++ src/app/components/shared/whatsapp-button.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { PhoneIcon as WhatsappIcon } from "lucide-react";
+import { useEffect } from "react";
 
 export function WhatsAppButton() {
-  return (
-    <motion.a
-      href="https://wa.me/3244099476" 
-      target="_blank"
-      rel="noopener noreferrer"
-      className="fixed bottom-20 right-4 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition-colors z-50"
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-    >
-      <WhatsappIcon className="h-6 w-6" />
-      <span className="sr-only">Contactar por WhatsApp</span>
-    </motion.a>
-  );
+  useEffect(() => {
+    // Cargar el script de Tidio
+    const script = document.createElement("script");
+    script.src = "//code.tidio.co/jvxcelve2juamau2qoqumgnva9izmuzz.js"; // Usamos el script proporcionado por Tidio
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Limpiar el script al desmontar el componente
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 }
